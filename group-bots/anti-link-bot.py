da6426d0c0c6e7230d2d02f9f86aaa0364d037c5
Easy-Quantum-Dev <mr7bbbbbbbb@gmail.com>
2026-09-21 20:38:34 +0430

    anti-link bot just complete

--- group-bots/anti-link-bot.py
+++ group-bots/anti-link-bot.py
@@ -1,19 +1,30 @@
 import telebot
+from telebot.types import ChatPermissions
+import datetime
+from datetime import timedelta
 token = 'TOKEN'
 bot = telebot.TeleBot(token)
+warnings = {}
+
 
 def is_admin(msg):
     user = bot.get_chat_member(msg.chat.id,msg.from_user.id)
     return user.status in [ "administrator","creator"]
 
-
+def add_warnings(chat_id,user_id):
+    key = (chat_id,user_id)
+    if key not in warnings:
+        warnings[key] =0
+    warnings[key]+=1
+    return warnings[key]    
+    
 
 @bot.message_handler(func=lambda msg :(
     msg.text is not None and not  msg.text.startswith("/")
     
 ))
 def anti_link(msg):
-    if is_admin(msg.chat.id,msg.from_user.id):
+    if is_admin(msg):
         return    
     text = msg.text.lower()
     link_words = [
@@ -28,7 +39,31 @@ def anti_link(msg):
     if not has_link:
         return
     try:
-        bot.delete_message(msg.chat.id,msg.message.id)
+        bot.delete_message(msg.chat.id,msg.message_id)
 
     except Exception:
         pass     
+    warnings_count = add_warnings(msg.chat.id,msg.from_user.id)
+    if warnings_count <3:
+        bot.send_message(msg.chat.id,f"""
+    🔗 لینک ممنوع است.
+    👤 کاربر:
+        {msg.from_user.first_name}
+
+        ⚠️ Warning:
+        {warnings_count}/3 """
+    )
+    else:
+        until_date  =(
+            datetime.datetime.now()+timedelta(minutes=10)
+        )
+        try:
+            bot.restrict_chat_member(msg.chat.id,msg.from_user.id,permissions=ChatPermissions(can_send_messages=False,can_send_other_messages=False),until_date=until_date)
+            warnings[msg.chat.id,msg.from_user.id]=0            
+        except ValueError as e:
+            bot.reply_to(msg,"عملیات انجام نشد")    
+        
+        
+        
+        
+    
